8b39eb9eda6a738853a4b5c91643d9e007223a1f
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-27 10:21:46 +0200

    Create basic article list view

--- src/articles/views.py
+++ src/articles/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+class ArticleListView(ListView):
+    def get(self, request):
+        return render(request, 'articles/article_list.html')
