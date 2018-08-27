2c2f0d83cd73d52a1b618a0dad9ce52c34ee731b
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-27 10:36:04 +0200

    Create, migrate and register 'Article' model

--- src/articles/models.py
+++ src/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title       = models.CharField(max_length=128)
+    slug        = models.SlugField()
+    body        = models.TextField()
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
