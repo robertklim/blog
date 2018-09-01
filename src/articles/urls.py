c348fb5ee9ad2c64364a69030f5a62a5f90cd6e7
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-01 09:12:28 +0200

    Create ArticleDetailView

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import ArticleDetailView, ArticleListView
 
 app_name = 'articles'
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
+    path('<slug:slug>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- src/articles/views.py
+++ src/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Article
+
+class ArticleDetailView(DetailView):
+    def get_queryset(self):
+        return Article.objects.all()
 
 class ArticleListView(ListView):
     def get_queryset(self):
