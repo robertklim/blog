48c5ca1d2de57c174fc70d68d65e050786d05fb4
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-02 13:48:33 +0200

    Create slug on save when no custom article slug is set

--- src/articles/models.py
+++ src/articles/models.py
@@ -3,7 +3,7 @@ from django.utils.text import slugify
 
 class Article(models.Model):
     title       = models.CharField(max_length=128)
-    slug        = models.SlugField(unique=True, blank=True)
+    slug        = models.SlugField(null=True, unique=True, blank=True)
     body        = models.TextField()
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
@@ -15,5 +15,6 @@ class Article(models.Model):
         return self.body[:50] + '...'
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        if self.slug is None:
+            self.slug = slugify(self.title)
         super(Article, self).save(*args, **kwargs)
