4653a185f1d53c913bcb5bf83cfdd22d0f037404
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-03 14:42:57 +0200

    Add user login

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AccountCreateView
+from .views import AccountCreateView, AccountLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='account-create'),
+    path('login/', AccountLoginView.as_view(), name='account-login'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 class AccountCreateView(CreateView):
     form_class = UserCreationForm
     template_name = 'accounts/account_create_form.html'
-    success_url = '/'
+    
+    def get_success_url(self):
+        return reverse_lazy('accounts:account-login')
+
+class AccountLoginView(LoginView):
+    template_name = 'accounts/account_login_form.html'
