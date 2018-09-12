18797a30d669c3ae670cec0368c2b8629863c966
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-12 11:05:58 +0200

    Add trailing slash in ArticleDetailView url and add cancel button in article_confirm_delete.html template

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -13,7 +13,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     path('create/', ArticleCreateView.as_view(), name='article-create'),
-    path('<slug:slug>', ArticleDetailView.as_view(), name='article-detail'),
+    path('<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
     path('<slug:slug>/edit/', ArticleUpdateView.as_view(), name='article-edit'),
     path('<slug:slug>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
 ]
