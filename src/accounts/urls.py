5edb0fee34c6bfe70b9e77472c6bf76faa3a75ee
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-16 18:48:22 +0200

    Move password reset urls to main project's urls file

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,9 +4,6 @@ from .views import (
     AccountCreateView,
     AccountLoginView, 
     AccountLogoutView,
-    AccountPasswordResetConfirmView,
-    AccountPasswordResetDoneView,
-    AccountPasswordResetView,
     AccountProfileView,
     profile_edit,
 )
@@ -17,9 +14,6 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='account-create'),
     path('login/', AccountLoginView.as_view(), name='account-login'),
     path('logout/', AccountLogoutView.as_view(), name='account-logout'),
-    path('password-reset/', AccountPasswordResetView.as_view(), name='account-password-reset'),
-    path('password-reset/done/', AccountPasswordResetDoneView.as_view(), name='account-password-reset-done'),
-    # path('password-reset-confirm/<uidb64>/<token>/', AccountPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('profile/', AccountProfileView.as_view(), name='account-profile'),
     path('profile/edit/', profile_edit, name='account-edit'),
 ]
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,4 +1,9 @@
-from accounts.views import AccountPasswordResetCompleteView, AccountPasswordResetConfirmView
+from accounts.views import (
+    AccountPasswordResetCompleteView,
+    AccountPasswordResetConfirmView,
+    AccountPasswordResetDoneView,
+    AccountPasswordResetView,
+)
 from articles.views import ArticleListView
 
 from django.conf import settings
@@ -16,6 +21,8 @@ urlpatterns = [
     path('articles/', include('articles.urls', namespace='articles')),
     path('password-reset-confirm/<uidb64>/<token>/', AccountPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password-reset-complete/', AccountPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/', AccountPasswordResetView.as_view(), name='account-password-reset'),
+    path('password-reset/done/', AccountPasswordResetDoneView.as_view(), name='account-password-reset-done'),
 ]
 
 if settings.DEBUG:
