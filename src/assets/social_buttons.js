9037f72865ff3888e5b664f6a03edf398ea15a0a
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-22 17:18:27 +0200

    Add social share buttons on article's detail page

---
+++ src/assets/social_buttons.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var options = {}
+    var instances = M.FloatingActionButton.init(elems, options);
+});
