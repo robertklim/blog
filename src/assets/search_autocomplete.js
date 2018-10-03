b608a17e62c263d3999424619f852d2fb99abef5
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-03 11:56:43 +0200

    Add autocomplete to searchbox (most common words from articles)

---
+++ src/assets/search_autocomplete.js
@@ -0,0 +1,24 @@
+var keywords_endpoint = '/api/articles/keywords/';
+var search_endpoint = '/articles/search/?q=';
+var autocomplete_keywords = {};
+
+fetch(keywords_endpoint)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        for (let i=0; i < myJson.length; i++) {
+            autocomplete_keywords[myJson[i][0]] = null;
+        }
+    });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.autocomplete');
+    var options = {
+        data: autocomplete_keywords,
+        onAutocomplete: function (val) {
+            location.href = search_endpoint + val;
+        },
+    }
+    var instances = M.Autocomplete.init(elems, options);
+});
